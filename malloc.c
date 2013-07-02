2b779c6db9d41469ea0021f9570f167793b85305
David Hovemeyer <david.hovemeyer@gmail.com>
2013-07-02 11:27:09 -0400

    new malloc/free implementation from scratch

--- malloc.c
+++ malloc.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2012 Bryan Cuccioli
+ * EasySandbox: an extremely simple sandbox for untrusted C/C++ programs
+ * Copyright (c) 2012,2013 David Hovemeyer <david.hovemeyer@gmail.com>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -20,161 +21,265 @@
  * SOFTWARE.
  */
 
-/*
- * Original source for this code:
- * https://github.com/bcuccioli/malloc
- */
-
-/* High-level overview: This works by maintaining a singly-linked list
- * of previously-used memory segments that have been freed. */
-
-#define NALLOC 1024
-
-#include <errno.h>
-#include <limits.h>
+#include <stdio.h>
 #include <string.h>
+#include <errno.h>
 #include <unistd.h>
 
-/* This header is stored at the beginning of memory segments in the list. */
-union header {
-  struct {
-    union header *next;
-    unsigned len;
-  } meta;
-  long x; /* Presence forces alignment of headers in memory. */
+/* Minimum amount of memory to allocate when we use sbrk to extend the heap */
+#define MIN_ALLOC 65536
+
+/* Block flags */
+#define ALLOCATED 1
+
+/* Header found at the beginning of each block */
+struct Header {
+	struct Header *prev, *next; /* previous and next blocks */
+	size_t size;                /* total size of block, including header */
+	int flags;                  /* block flags */
 };
 
-static union header list;
-static union header *first = NULL;
+/* List of blocks, sorted by order of increasing addresses */
+static struct Header *s_head, *s_tail;
 
-void free(void* ptr) {
-  if (ptr == NULL) {
-    return;
-  }
-
-  union header *iter, *block;
-  iter = first;
-  block = (union header*)ptr - 1;
-
-  /* Traverse to the spot in the list to insert the freed fragment,
-   * such that the list is ordered by memory address (for coalescing). */
-  while (block <= iter || block >= iter->meta.next) {
-    if ((block > iter || block < iter->meta.next) &&
-        iter >= iter->meta.next) {
-      break;
-    }
-    iter = iter->meta.next;
-  }
-
-  /* If the new fragment is adjacent in memory to any others, merge
-   * them (we only have to check the adjacent elements because the
-   * order semantics are enforced). */
-  if (block + block->meta.len == iter->meta.next) {
-    block->meta.len += iter->meta.next->meta.len;
-    block->meta.next = iter->meta.next->meta.next;
-  } else {
-    block->meta.next = iter->meta.next;
-  }
-  if (iter + iter->meta.len == block) {
-    iter->meta.len += block->meta.len;
-    iter->meta.next = block->meta.next;
-  } else {
-    iter->meta.next = block;
-  }
-
-  first = iter;
+/*
+ * Round given size up to the nearest multiple.
+ */
+static inline size_t round_to_multiple(size_t n, size_t multiple)
+{
+	if (n % multiple != 0) {
+		n += multiple - (n % multiple);
+	}
+	return n;
 }
 
-void *malloc(size_t size) {
-  union header *p, *prev;
-  prev = first;
-  unsigned true_size =
-    (size + sizeof(union header) - 1) /
-     sizeof(union header) + 1;
-
-  /* If the list of previously allocated fragments is empty,
-   * initialize it. */
-  if (first == NULL) {
-    prev = &list;
-    first = prev;
-    list.meta.next = first;
-    list.meta.len = 0;
-  }
-  p = prev->meta.next;
-  /* Traverse the list of previously allocated fragments, searching
-   * for one sufficiently large to allocate. */
-  while (1) {
-    if (p->meta.len >= true_size) {
-      if (p->meta.len == true_size) {
-        /* If the fragment is exactly the right size, we do not have
-         * to split it. */
-        prev->meta.next = p->meta.next;
-      } else {
-        /* Otherwise, split the fragment, returning the first half and
-         * storing the back half as another element in the list. */
-        p->meta.len -= true_size;
-        p += p->meta.len;
-        p->meta.len = true_size;
-      }
-      first = prev;
-      return (void *)(p+1);
-    }
-    /* If we reach the beginning of the list, no satisfactory fragment
-     * was found, so we have to request a new one. */
-    if (p == first) {
-      char *page;
-      union header *block;
-      /* We have to request memory of at least a certain size. */
-      if (true_size < NALLOC) {
-        true_size = NALLOC;
-      }
-      page = sbrk((intptr_t) (true_size * sizeof(union header)));
-      if (page == (char *)-1) {
-        /* There was no memory left to allocate. */
-        errno = ENOMEM;
-        return NULL;
-      }
-      /* Create a fragment from this new memory and add it to the list
-       * so the above logic can handle breaking it if necessary. */
-      block = (union header *)page;
-      block->meta.len = true_size * sizeof(union header);
-      free((void *)(block + 1));
-      p = first;
-    }
-
-    prev = p;
-    p = p->meta.next;
-  }
-  return NULL;
+/*
+ * Predicate to test whether given block is allocated.
+ */
+static inline int is_allocated(struct Header *block)
+{
+	return (block->flags & ALLOCATED) != 0;
 }
 
-void* calloc(size_t num, size_t len) {
-  void* ptr = malloc(num * len);
+/*
+ * Allocate a new block using sbrk and append it to the list of blocks.
+ * Returns the pointer to the allocated block, or null if
+ * sbrk couldn't allocate more memory
+ */
+static struct Header *alloc_block(size_t block_size)
+{
+	struct Header *block;
 
-  /* Set the allocated array to 0's.*/
-  if (ptr != NULL) {
-    memset(ptr, 0, num * len);
-  }
+	/* ensure minimum allocation size */
+	if (block_size < MIN_ALLOC) {
+		block_size = MIN_ALLOC;
+	}
 
-  return ptr;
+	/* use sbrk to extend the heap */
+	block = sbrk((intptr_t) block_size);
+	if (block == (void *)-1) {
+		return 0;
+	}
+
+	/* Append block to list */
+	block->next = 0;
+	if (s_head == 0) {
+		/* first allocation */
+		s_head = s_tail = block;
+		block->prev = 0;
+	} else {
+		/* append block at tail of list */
+		s_tail->next = block;
+		block->prev = s_tail;
+		s_tail = block;
+	}
+	block->size = block_size;
+	block->flags = 0;
+
+	return block;
 }
 
-void* realloc(void *ptr, size_t new_size) {
-  size_t old_size = 0; /* XXX yolo */
-  void* new = malloc(new_size);
+/*
+ * Split given block if its excess space beyond given required block size
+ * is large enough to form a useful block.
+ */
+static void split_block_if_necessary(struct Header *block, size_t required_block_size)
+{
+	struct Header *excess;
+	size_t left_over;
 
-  if (new == NULL) {
-    /* malloc() failed, insufficient memory remaining. */
-    errno = ENOMEM;
-  } else {
-    /* We cannot grow the memory segment. */
-    if (new_size > old_size) {
-      new_size = old_size;
-    }
-    memmove(new, ptr, new_size);
-    free(ptr);
-  }
+	/* is there enough room to form a useful block (larger than just a header)? */
+	left_over = block->size - required_block_size;
+	if (left_over <= sizeof(struct Header)) {
+		return;
+	}
 
-  return new;
+	/* adjust size of the original block */
+	block->size = required_block_size;
+
+	/* compute address of block formed from excess memory in block */
+	excess = (struct Header *) (((char *) block) + required_block_size);
+
+	/* initialize the new block's header */
+	excess->size = left_over;
+	excess->flags = 0;
+
+	/* graft the new block into the list as current block's successor */
+	excess->next = block->next;
+	excess->prev = block;
+	if (block->next != 0) {
+		block->next->prev = excess;
+	} else {
+		/* splitting the tail block, so excess is new tail */
+		s_tail = excess;
+	}
+	block->next = excess;
 }
 
+/*
+ * Coalesce given block with its successor if necesary.
+ */
+static void coalesce_if_necessary(struct Header *block)
+{
+	/* check whether successor exists and both block and successor are free */
+	if (is_allocated(block) || block->next == 0 || is_allocated(block->next)) {
+		return;
+	}
+
+	/* absorb successor into block */
+	block->size += block->next->size;
+
+	/* splice successor out of the list */
+	if (block->next->next != 0) {
+		block->next->prev = block;
+	} else {
+		/* successor was the tail block, so block becomes tail */
+		s_tail = block;
+	}
+	block->next = block->next->next;
+}
+
+/*
+ * Allocate a buffer of given size.
+ */
+void *malloc(size_t size)
+{
+	size_t required_block_size;
+	struct Header *block;
+
+	/* calculate the minimum block size needed for this allocation */
+	required_block_size = round_to_multiple(size + sizeof(struct Header), sizeof(struct Header));
+
+	/* search list for an unallocated block of sufficient size */
+	for (block = s_head; block != 0; block = block->next) {
+		if (block->size >= required_block_size && !is_allocated(block)) {
+			break;
+		}
+	}
+
+	/* if no sufficiently-large block was found, allocate one and append it to list */
+	if (block == 0) {
+		block = alloc_block(required_block_size);
+		if (block == 0) {
+			/* failed to allocate a new block */
+			errno = ENOMEM;
+			return 0;
+		}
+	}
+
+	/* if block size exceeds required block size by more than the size of one header,
+	 * then split it */
+	split_block_if_necessary(block, required_block_size);
+
+	/* mark the block as allocated */
+	block->flags |= ALLOCATED;
+
+	return (void*) (block + 1);
+}
+
+/*
+ * Free a buffer allocated with malloc.
+ */
+void free(void *p)
+{
+	struct Header *block;
+
+	if (p == 0) {
+		return;
+	}
+
+	/* find header */
+	block = ((struct Header *)p) - 1;
+
+	/* ensure that this is actually an allocated block */
+	if (!is_allocated(block)) {
+		fprintf(stderr, "Invalid free at %p\n", p);
+		return;
+	}
+
+	/* mark block as being free */
+	block->flags &= ~(ALLOCATED);
+
+	/* Attempt to coalesce with predecessor and successor blocks */
+	coalesce_if_necessary(block);
+	coalesce_if_necessary(block->prev);
+}
+
+/*
+ * Allocate a zeroed buffer.
+ */
+void *calloc(size_t nmemb, size_t size)
+{
+	void *buf;
+
+	buf = malloc(nmemb * size);
+	if (buf != 0) {
+		memset(buf, 0, nmemb * size);
+	}
+	return buf;
+}
+
+/*
+ * Reallocate given buffer so that it has given size.
+ */
+void *realloc(void *ptr, size_t size)
+{
+	struct Header *block;
+	size_t to_copy;
+	void *buf;
+
+	/* special case: if ptr is null, then allocate a new buffer
+	 * using malloc */
+	if (ptr == 0) {
+		return malloc(size);
+	}
+
+	/* special case: if size is 0, then free the buffer */
+	if (size == 0) {
+		free(ptr);
+		return 0;
+	}
+
+	/* find buffer's block header */
+	block = ((struct Header *)ptr) - 1;
+
+	/* allocate a new buffer */
+	buf = malloc(size);
+	if (buf == 0) {
+		return 0;
+	}
+
+	/* copy data from old buffer to new buffer */
+	to_copy = block->size - sizeof(struct Header); /* original buffer size */
+	if (to_copy > size) {
+		/* original size was larger than new size */
+		to_copy = size;
+	}
+	memcpy(buf, ptr, to_copy);
+
+	/* free the old buffer */
+	free(ptr);
+
+	/* return the new buffer */
+	return buf;
+}
